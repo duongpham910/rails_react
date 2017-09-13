a83f34900b9741682412f2daa7b96be821d5d136
duongpham910 <duongpham91094@gmail.com>
2017-09-13 16:48:14 +0700

    finish create task

--- client/src/API/Task.jsx
+++ client/src/API/Task.jsx
@@ -1,9 +1,19 @@
-import BaseAPI from "./BaseAPI";
+import BaseAPI from './BaseAPI';
 
 export default class Task extends BaseAPI {
   static getListTask(callback) {
     this.sendAjax(callback, {
-      url: "tasks"
+      url: 'tasks'
+    });
+  }
+
+  static createTask(callback, object, options = {}) {
+    let objectName = "task";
+    let data = update(options, {$merge: {[objectName]: object}})
+    this.sendAjax(callback, {
+      url: 'tasks',
+      method: 'POST',
+      data: data
     });
   }
 }
