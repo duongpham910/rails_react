44f2c24a1a0ebc171d7a9d6a3a844200b34d3973
duongpham910 <duongpham91094@gmail.com>
2017-09-26 14:36:49 +0700

    validate, UI/UX

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
   validates :title, :due_date, :priority, presence: true
 
+  validates :estimate_time, numericality: {greater_than_or_equal_to: 0}
+  validates :description, length: {maximum: 255}
+
   enum priority: [:Low, :Medium, :High, :Urgent]
   enum status: [:New, :Inprogress, :Completed, :Reopen]
 
---
+++ client/src/Helper/AlertDialog.jsx
@@ -0,0 +1,58 @@
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
+export default class AlertDialog extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      title: '',
+      content: '',
+    }
+  }
+
+  open(title, content, onSubmit) {
+    this.setState({
+      open: true,
+      title: title,
+      content: content,
+      onSubmit: onSubmit,
+    })
+  }
+
+  handleClose = () => {
+    let open = update(this.state.open, {$set: false});
+    this.setState({
+      open: open,
+    })
+  };
+
+  handleSubmit = () => {
+    this.state.onSubmit();
+    this.handleClose();
+  };
+
+  render() {
+    const actions = [
+      <FlatButton
+        label={'ok'}
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleSubmit}
+      />
+    ];
+
+    return (
+      <Dialog
+        title={this.state.title}
+        onRequestClose={this.handleClose}
+        open={this.state.open}
+        actions={actions}
+        modal={true}
+        style={{zIndex: 10000}}
+      >
+        {this.state.content}
+      </Dialog>
+    );
+  }
+}
